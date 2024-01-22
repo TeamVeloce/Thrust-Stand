b6af609eac1e0b89e42e8aa2fc8d9f15078127e8
Mihir-Dixit-0365 <141617925+Mihir-Dixit-0365@users.noreply.github.com>
2024-01-23 00:31:15 +0530

    Update test_plot.py

--- Scripts/test_plot.py
+++ Scripts/test_plot.py
@@ -3,10 +3,19 @@ import serial
 
 ser = serial.Serial('COM32 ',9600)
 fig1 = plt.figure(figsize=(7,3))
+fig2 = plt.figure(figsize=(7,3))
+fig3 = plt.figure(figsize=(7,3))
+fig4 = plt.figure(figsize=(7,3))
 
 ax1 = fig1.add_subplot()
+ax2 = fig2.add_subplot()
+ax3 = fig3.add_subplot()
+ax4 = fig4.add_subplot()
 
 fig1.show()
+fig2.show()
+fig3.show()
+fig4.show()
 
 a1=[]
 
@@ -15,7 +24,7 @@ thrust_vals = []
 rpm_vals = []
 voltage_vals = []
 
-x_vals=[0]
+x_vals=[0,0,0,0]
 i=0
 l=200
 ser.close()
@@ -33,14 +42,38 @@ for i in range(l):
     rpm_vals.append(a1[0])
     voltage_vals.append(a1[3])  
     #print(current_vals[ctr])
+    #print(voltage_vals[ctr])
     
-    plt.xlabel('current(mA)', fontweight='bold', horizontalalignment='center')
-    plt.ylabel('RPM', fontweight='bold', horizontalalignment='center')
-    ax1.plot(current_vals[ctr],voltage_vals[ctr],color='b')
+    x_vals[0] =i
+    plt.ylabel('current(mA)', fontweight='bold', horizontalalignment='center')
+    #plt.ylabel('RPM', fontweight='bold', horizontalalignment='center')
+    ax1.plot(current_vals,color='b')
     fig1.canvas.draw()
-    ax1.set_xlim(left=max(0,x_vals[0]-5), right=x_vals[0]+10)
-    ctr = ctr+1
+    ax1.set_xlim(left=max(0,x_vals[0]-30), right=x_vals[0]+60)
     
+    x_vals[1] =i
+    plt.ylabel('Voltage(V)', fontweight='bold', horizontalalignment='center')
+    #plt.ylabel('RPM', fontweight='bold', horizontalalignment='center')
+    ax2.plot(voltage_vals,color='b')
+    fig2.canvas.draw()
+    ax2.set_xlim(left=max(0,x_vals[1]-30), right=x_vals[1]+60)
+    
+    x_vals[2] =i
+    plt.ylabel('RPM', fontweight='bold', horizontalalignment='center')
+    #plt.ylabel('RPM', fontweight='bold', horizontalalignment='center')
+    ax3.plot(rpm_vals,color='b')
+    fig3.canvas.draw()
+    ax3.set_xlim(left=max(0,x_vals[2]-30), right=x_vals[2]+60)
+    
+    x_vals[3] =i
+    plt.ylabel('Thrust', fontweight='bold', horizontalalignment='center')
+    #plt.ylabel('RPM', fontweight='bold', horizontalalignment='center')
+    ax4.plot(thrust_vals,color='b')
+    fig4.canvas.draw()
+    ax4.set_xlim(left=max(0,x_vals[3]-30), right=x_vals[3]+60)
+    
+    ctr = ctr+1
+        
     plt.pause(0.0001)
 
 plt.show()
