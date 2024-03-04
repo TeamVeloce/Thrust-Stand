f18c1febee3a5fa591bc8d60f8ee3fd37d28e468
ShreyasDevesh007 <154617760+ShreyasDevesh007@users.noreply.github.com>
2024-03-04 22:22:03 +0530

    Update LOAD_CELL.ino

--- Scripts/Independent_Sensors/LOAD_CELL.ino
+++ Scripts/Independent_Sensors/LOAD_CELL.ino
@@ -1,72 +1,40 @@
-#include <HX711_ADC.h>
-#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
-#include <EEPROM.h>
-#endif
+#include "HX711.h"
 
-//pins:
-const int HX711_dout = 4; //mcu > HX711 dout pin
-const int HX711_sck = 5; //mcu > HX711 sck pin
+#define DOUT_PIN  7
+#define CLK_PIN   8
 
-//HX711 constructor:
-HX711_ADC LoadCell(HX711_dout, HX711_sck);
-
-const int calVal_eepromAdress = 0;
-unsigned long t = 0;
+HX711 scale;
 
 void setup() {
-  Serial.begin(57600); delay(10);
-  Serial.println();
-  Serial.println("Starting...");
-
-  LoadCell.begin();
-  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-  float calibrationValue; // calibration value (see example file "Calibration.ino")
-  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
-#if defined(ESP8266)|| defined(ESP32)
-  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
-#endif
-  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
-
-  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
-  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
-  LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag()) {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-    while (1);
-  }
-  else {
-    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
-    Serial.println("Startup is complete");
-  }
+  Serial.begin(9600);
+  
+  // Initialize the HX711 ADC
+  scale.begin(DOUT_PIN, CLK_PIN);
+  
+  // Set the scale to gain 128 (default is 128)
+  scale.set_scale();
+  
+  // Tare the scale
+  scale.tare();
 }
 
 void loop() {
-  static boolean newDataReady = 0;
-  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+  // Read the raw ADC value
+  long rawValue = scale.read();
+  //Serial.println(rawValue);
 
-  // check for new data/start next conversion:
-  if (LoadCell.update()) newDataReady = true;
+  // Convert the raw value to mass in grams
+  // Use the calibration factor for your specific load cell
+  // You can obtain the calibration factor by dividing the known weight in grams by the raw value
+  // For example, if a known weight of 100 grams produces a raw value of 1000, then the calibration factor would be 100 / 1000 = 0.1
+  float calibrationFactor = 0.00332; // Adjust this value according to your calibration
+  float massInGrams = rawValue * calibrationFactor;
+  float final_mass = 270 - massInGrams;
 
-  // get smoothed value from the dataset:
-  if (newDataReady) {
-    if (millis() > t + serialPrintInterval) {
-      float i = LoadCell.getData();
-      Serial.print("Load_cell output val: ");
-      Serial.println(i);
-      newDataReady = 0;
-      t = millis();
-    }
-  }
+  // Print the mass in grams
+  Serial.print("Mass: ");
+  Serial.print(final_mass);
+  Serial.println(" grams");
 
-  // receive command from serial terminal, send 't' to initiate tare operation:
-  if (Serial.available() > 0) {
-    char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay();
-  }
-
-  // check if last tare operation is complete:
-  if (LoadCell.getTareStatus() == true) {
-    Serial.println("Tare complete");
-  }
-
+  delay(1000); // Delay for stability
 }
